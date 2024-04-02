214d6d804e1a1538b8a69baa43406f5b9961e5fb
William Bojczuk <williambojczuk@gmail.com>
2024-04-02 19:08:45 +0200

    updated stuff

--- src/app/(mainsite)/components/misc/Footer/Footer.tsx
+++ src/app/(mainsite)/components/misc/Footer/Footer.tsx
@@ -48,6 +48,11 @@ export default function Footer() {
                 <InlineIcon icon={"mdi:email"} width="50px" height="50px" />
                   <a href={"mailto:" + process.env.NEXT_PUBLIC_EMAIL_ADDRESS} target="_blank">{process.env.NEXT_PUBLIC_EMAIL_ADDRESS}</a>
                 </aside>
+
+                <aside className={`${styles.contactItem} ${styles.listItem}`}>
+                <InlineIcon icon={"mdi:phone"} width="50px" height="50px" />
+                  <a href={"tel:" + process.env.NEXT_PUBLIC_PHONE_NUM} target="_blank">{process.env.NEXT_PUBLIC_PHONE_NUM_FORMATTED}</a>
+                </aside>
                 
                 <aside className={`${styles.contactItem} ${styles.listItem}`}>
                 <InlineIcon icon={"mdi:map-marker"} width="50px" height="50px" />
