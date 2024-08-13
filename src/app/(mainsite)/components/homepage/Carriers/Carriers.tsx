7bf9cb01c3bf85e25903bb674fda55c81e378fbd
William Bojczuk <williambojczuk@gmail.com>
2024-08-12 21:56:19 -0400

    updated

--- src/app/(mainsite)/components/homepage/Carriers/Carriers.tsx
+++ src/app/(mainsite)/components/homepage/Carriers/Carriers.tsx
@@ -3,7 +3,7 @@ import styles from './carriers.module.css';
 export default function Carriers(){
 return (
  <div className={styles.carriers}>
-    <h2>6+ Carriers!</h2>
-    <h4>In Alabama and Michigan</h4>
+    <h2>15+ Carriers!</h2>
+    <h4>In Michigan</h4>
  </div>
 )};
